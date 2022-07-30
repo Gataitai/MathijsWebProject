7b2bc73ea319fc49c98995c03f52aa491299c2ad
mpattipeilohy <512391@student.saxion.nl>
2022-07-30 16:48:59 +0200

    finished pixelartposts comment area

--- src/main/resources/newFrontend/js/components/Card.js
+++ src/main/resources/newFrontend/js/components/Card.js
@@ -115,4 +115,83 @@ function newPixelArtPostCard(post) {
 function newPixelArtPostCommentSectionCard(post, comments) {
   let card = document.createElement("div");
   card.classList.add("card");
+  card.style.width = "50vw";
+
+  let header = document.createElement("div");
+  header.classList.add("card-header");
+
+  let img = document.createElement("img");
+  img.classList.add("smallPersonImage");
+  img.src = post.person.photoLink;
+  header.appendChild(img);
+  header.appendChild(document.createTextNode(post.person.name));
+  card.appendChild(header);
+
+  let row = document.createElement("div");
+  row.classList.add("row");
+
+  let pixelArt = document.createElement("div");
+  pixelArt.classList.add("col-8");
+  pixelArt.classList.add("pixelArt");
+  for(let pxl of post.pixelArtAsJSON.grid){
+    let pixel = document.createElement("div");
+    pixel.classList.add("pixel");
+    pixel.style.backgroundColor = pxl.color;
+    pixelArt.appendChild(pixel);
+  }
+  row.appendChild(pixelArt);
+
+  let col4 = document.createElement("div");
+  col4.classList.add("col-4");
+
+  let cardBody = document.createElement("div");
+  cardBody.classList.add("card-body");
+  cardBody.classList.add("commentSection");
+
+  for(let cmt of comments){
+    for (let i = 0; i < 100; i++) {
+      let comment = document.createElement("div");
+      comment.classList.add("mb-2");
+      
+      let img = document.createElement("img");
+      img.classList.add("smallPersonImage");
+      img.src = cmt.person.photoLink;
+  
+      comment.appendChild(img);
+      comment.appendChild(document.createTextNode(cmt.person.name + ": " + cmt.text));
+
+      let date = document.createElement("p");
+      date.classList.add("text-muted");
+      date.appendChild(document.createTextNode("july 18th 2008"));
+
+      comment.appendChild(date);
+      
+      cardBody.appendChild(comment);
+    }
+  }
+
+  col4.appendChild(cardBody);
+  row.appendChild(col4);
+
+  card.appendChild(row);
+  return card;
 }
+
+/* <div class="card" style="width: 50vw;">
+<div class="card-header">
+  <img class="smallPersonImage" src="https://cdn.discordapp.com/attachments/1001277448329109666/1001277877247017001/1.png">Mathijs
+</div>
+<div class="row">
+  <div class="col-8 pixelArt">
+    <div class="pixel" style="background-color: rgb(139, 180, 195);"></div>
+  ...
+  </div>
+  <div class="col-4">
+    <div class="card-body commentSection">
+      <div class="mb-2">
+        <img class="smallPersonImage" src="https://cdn.discordapp.com/attachments/1001277448329109666/1001277877247017001/1.png">Mathijs: Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus magnam est illum, officiis rem aliquam assumenda recusandae eaque alias, commodi architecto, incidunt obcaecati consectetur soluta vitae dolorum facilis repellat. Reprehenderit, eveniet rem. Amet nihil facere perferendis, assumenda cum dolorem exercitationem rerum! Beatae, eius quae! Tenetur fugiat provident nihil alias sequi.
+      </div>
+    </div>
+  </div>
+</div>
+</div> */
